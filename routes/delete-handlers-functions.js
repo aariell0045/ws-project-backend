f8d747de55b82418c7f32315009a815b93b2bb2f
toam <toamcohen10@gmail.com>
2021-09-22 02:59:52 +0300

    done with messages for today ty

--- routes/delete-handlers-functions.js
+++ routes/delete-handlers-functions.js
@@ -5,65 +5,68 @@ async function deletemessage(req, res) {
 	let { messageId, userId } = req.body;
 	const currentUser = await User.findOne({ _id: userId });
 
-	let messageIndex = currentUser.messages.findIndex(message => message._id == messageId);
+	let messageIndex = currentUser.messages.findIndex((message) => message._id == messageId);
 	if (messageIndex !== -1) {
-		
-		currentUser.messages.splice(messageIndex, 1)
+		currentUser.messages.splice(messageIndex, 1);
 		console.log(currentUser.messages);
-		await User.findByIdAndUpdate({ _id: userId }, {
+		await User.findByIdAndUpdate(
+			{ _id: userId },
+			{
 				$set: {
-						messages: currentUser.messages
-				}
-			})
-		}
+					messages: currentUser.messages,
+				},
+			}
+		);
+	}
 
-	res.status(200).json("deleted")
-	
+	res.status(200).json("deleted");
 }
 
 async function removeGroup(req, res) {
 	let { userId, groupId } = req.body;
 
 	await User.findById({ _id: userId }, function (err, currentUser) {
-		const groupIndex = currentUser.groups.findIndex(group => group._id == groupId);
+		const groupIndex = currentUser.groups.findIndex((group) => group._id == groupId);
 		if (groupIndex !== -1) {
-			
 			currentUser.groups.splice(groupIndex, 1);
 			currentUser.save(function (err) {
-
 				if (err) {
-
 					res.status(400).json("Error:", err);
 				} else {
-					
 					res.status(200).json(currentUser);
-
 				}
-			})
+			});
 		} else {
 			res.status(400).json("something worng");
 		}
-	})
-};
- 
-async function removeContactFromGroup(req, res) {
-	let { userId, groupId, contactId } = req.body;
-	const currentUser = await User.findOne({ _id: userId });
-	const groupIndex = currentUser.groups.findIndex(group => group._id == groupId);
-	const contactIndex = currentUser.groups[groupIndex].contacts.findIndex(contact => contact._id == contactId);
-	currentUser.groups[groupIndex].contacts.splice(contactIndex, 1);
-
-	await User.findByIdAndUpdate({ _id: userId }, {
-		$set: {
-			groups: currentUser.groups
-		}
-	})
-
-	res.status(200).json(currentUser)
-
-	
+	});
 }
 
+async function removeContactFromGroup(req, res) {
+	let { userId, groupId, contactId } = req.body;
+	console.log("userId:", userId);
+	console.log("groupId:", groupId);
+	console.log("contactId:", contactId);
+	const currentUser = await User.findOne({ _id: userId });
+	const groupIndex = currentUser.groups.findIndex((group) => group._id == groupId);
+
+	const contactIndex = currentUser.groups[groupIndex].contacts.findIndex(
+		(contact) => contact._id == contactId
+	);
+	currentUser.groups[groupIndex].contacts.splice(contactIndex, 1);
+	currentUser.groups[groupIndex].amount -= 1;
+
+	await User.findByIdAndUpdate(
+		{ _id: userId },
+		{
+			$set: {
+				groups: currentUser.groups,
+			},
+		}
+	);
+
+	res.status(200).json(currentUser);
+}
 
 const deleteHandlers = { deletemessage, removeGroup, removeContactFromGroup };
 
--- routes/put-handlers-functions.js
+++ routes/put-handlers-functions.js
@@ -5,198 +5,184 @@ const Contact = require("../Schema/Contact");
 const helperFunctions = require("../helper/helperFunctions");
 
 async function addMessage(req, res) {
-  let { messageName, messagesList, userId } = req.body;
+	let { messageName, contentMessage, userId } = req.body;
 
-  let date = new Date();
-  let year = date.getFullYear();
-  let month = date.getMonth();
-  let day = date.getDate() + 1;
+	let newMessage = await new Message({
+		messageName,
+		contentMessage,
+	});
 
-  console.log(`${day}/${month}/${year}`);
-  let messageDate = `${day}/${month}/${year}`;
-  let newMessage = await new Message({
-    messageName,
-    messageDate,
-    messagesList,
-  });
+	await User.findByIdAndUpdate(
+		{ _id: userId },
+		{
+			$push: {
+				messages: newMessage,
+			},
+		}
+	);
 
-  await User.findByIdAndUpdate(
-    { _id: userId },
-    {
-      $push: {
-        messages: newMessage,
-      },
-    }
-  );
-
-  res.status(200).json("ani gever");
+	res.status(200).json(newMessage);
 }
 
 async function editMessage(req, res) {
-  let { messageId, userId, messageName, messagesList } = req.body;
-  const currentUser = await User.findById({ _id: userId });
-  let messageIndex = currentUser.messages.findIndex(
-    (message) => message._id == messageId
-  );
-  if (messageIndex !== -1) {
-    currentUser.messages.splice(messageIndex, 1, {
-      ...currentUser.messages[messageIndex],
-      messageName,
-      messagesList,
-    });
-    console.log(currentUser.messages);
-    await User.findByIdAndUpdate(
-      { _id: userId },
-      {
-        $set: {
-          messages: currentUser.messages,
-        },
-      }
-    );
-    res.status(200).json(currentUser.messages[messageIndex]);
-  } else {
-    res.status(404).json("message not found");
-  }
+	let { messageId, userId, messageName, contentMessage } = req.body;
+	const currentUser = await User.findById({ _id: userId });
+	let messageIndex = currentUser.messages.findIndex((message) => message._id == messageId);
+	if (messageIndex !== -1) {
+		currentUser.messages.splice(messageIndex, 1, {
+			...currentUser.messages[messageIndex],
+			messageName,
+			contentMessage,
+		});
+		console.log(currentUser.messages);
+		await User.findByIdAndUpdate(
+			{ _id: userId },
+			{
+				$set: {
+					messages: currentUser.messages,
+				},
+			}
+		);
+		res.status(200).json(currentUser.messages[messageIndex]);
+	} else {
+		res.status(404).json("message not found");
+	}
 
-  // we have option to change the messageID to the message index when we will build the front;
+	// we have option to change the messageID to the message index when we will build the front;
 }
 
 async function uploadExcelfile(req, res) {
-  let { excelFile, profile, userId, groupName } = req.body;
-  let { gender } = profile;
-  if (gender && gender !== "without-gender") {
-    excelFile = excelFile.filter((row) => {
-      gender = gender.toLowerCase();
-      let currentGender = row?.gender;
-      currentGender = currentGender?.toLowerCase();
-      if (currentGender === gender) {
-        return true;
-      }
-      return false;
-    });
-  }
-  addGroup(userId, groupName, excelFile, res);
+	let { excelFile, profile, userId, groupName } = req.body;
+	let { gender } = profile;
+	if (gender && gender !== "without-gender") {
+		excelFile = excelFile.filter((row) => {
+			gender = gender.toLowerCase();
+			let currentGender = row?.gender;
+			currentGender = currentGender?.toLowerCase();
+			if (currentGender === gender) {
+				return true;
+			}
+			return false;
+		});
+	}
+	addGroup(userId, groupName, excelFile, res);
 }
 
 async function addGroup(userId, groupName, contacts, res) {
-  let duplicate = {};
-  let newContacts = [];
-  for await (contact of contacts) {
-    let phoneNumber = helperFunctions.phoneFormater(contact.phoneNumber);
-    if (phoneNumber.properNumber && !duplicate[phoneNumber.phone]) {
-      duplicate[phoneNumber.phone] = true;
-      let newContact = await new Contact({
-        phoneNumber: phoneNumber.phone,
-        contactProfile: contact.contactProfile,
-      });
-      newContacts.push(newContact);
-    }
-  }
-  contacts = newContacts;
+	let duplicate = {};
+	let newContacts = [];
+	for await (contact of contacts) {
+		let phoneNumber = helperFunctions.phoneFormater(contact.phoneNumber);
+		if (phoneNumber.properNumber && !duplicate[phoneNumber.phone]) {
+			duplicate[phoneNumber.phone] = true;
+			let newContact = await new Contact({
+				phoneNumber: phoneNumber.phone,
+				contactProfile: contact.contactProfile,
+			});
+			newContacts.push(newContact);
+		}
+	}
+	contacts = newContacts;
 
-  const dateObject = new Date();
-  const year = dateObject.getFullYear();
-  const month = dateObject.getMonth() + 1;
-  const day = dateObject.getDate();
-  const productionDate = `${day}/${month}/${year}`;
+	const dateObject = new Date();
+	const year = dateObject.getFullYear();
+	const month = dateObject.getMonth() + 1;
+	const day = dateObject.getDate();
+	const productionDate = `${day}/${month}/${year}`;
 
-  const currentUser = await User.findOne({ _id: userId });
-  let newGroup = await new Group({
-    groupName,
-    contacts,
-    productionDate,
-    amount: contacts.length,
-  });
-  currentUser.groups.push(newGroup);
+	const currentUser = await User.findOne({ _id: userId });
+	let newGroup = await new Group({
+		groupName,
+		contacts,
+		productionDate,
+		amount: contacts.length,
+	});
+	currentUser.groups.push(newGroup);
 
-  await User.findByIdAndUpdate(
-    { _id: userId },
-    {
-      $set: {
-        groups: currentUser.groups,
-      },
-    },
-    { new: true },
-    function (err, groups) {
-      if (err) {
-        return res.status(400).json("something went worng");
-      }
-      return res.status(200).json(groups);
-    }
-  );
+	await User.findByIdAndUpdate(
+		{ _id: userId },
+		{
+			$set: {
+				groups: currentUser.groups,
+			},
+		},
+		{ new: true },
+		function (err, groups) {
+			if (err) {
+				return res.status(400).json("something went worng");
+			}
+			return res.status(200).json(groups);
+		}
+	);
 }
 
 async function editGroup(req, res) {
-  let { userId, groupId, contactId, newContact, newGroupName } = req.body;
+	let { userId, groupId, contactId, newContact, newGroupName } = req.body;
 
-  const currentUser = await User.findOne({ _id: userId });
-  if (currentUser) {
-    let groupToEditIndex = currentUser.groups.findIndex(
-      (group) => group._id == groupId
-    );
+	const currentUser = await User.findOne({ _id: userId });
+	if (currentUser) {
+		let groupToEditIndex = currentUser.groups.findIndex((group) => group._id == groupId);
 
-    if (groupToEditIndex !== -1) {
-      let contactToEditIndex = currentUser.groups[
-        groupToEditIndex
-      ].contacts.findIndex((contact) => contact._id == contactId);
+		if (groupToEditIndex !== -1) {
+			let contactToEditIndex = currentUser.groups[groupToEditIndex].contacts.findIndex(
+				(contact) => contact._id == contactId
+			);
 
-      if (contactToEditIndex !== -1 && newContact) {
-        currentUser.groups[groupToEditIndex].contacts[contactToEditIndex] = {
-          ...currentUser.groups[groupToEditIndex].contacts[contactToEditIndex],
-          ...newContact,
-        };
-      }
-      if (newGroupName) {
-        currentUser.groups[groupToEditIndex].groupName = newGroupName;
-      }
-    }
-    await User.findByIdAndUpdate(
-      { _id: userId },
-      {
-        $set: {
-          groups: currentUser.groups,
-        },
-      }
-    );
-  }
-  res.status(200).json(currentUser.groups);
+			if (contactToEditIndex !== -1 && newContact) {
+				currentUser.groups[groupToEditIndex].contacts[contactToEditIndex] = {
+					...currentUser.groups[groupToEditIndex].contacts[contactToEditIndex],
+					...newContact,
+				};
+			}
+			if (newGroupName) {
+				currentUser.groups[groupToEditIndex].groupName = newGroupName;
+			}
+		}
+		await User.findByIdAndUpdate(
+			{ _id: userId },
+			{
+				$set: {
+					groups: currentUser.groups,
+				},
+			}
+		);
+	}
+	res.status(200).json(currentUser.groups);
 }
 
 async function addContactToGroup(req, res) {
-  let { userId, newContact, groupId } = req.body;
+	let { userId, newContact, groupId } = req.body;
 
-  const currentUser = await User.findOne({ _id: userId });
-  const groupIndex = currentUser.groups.findIndex(
-    (group) => group._id == groupId
-  );
-  if (groupIndex !== -1) {
-    currentUser.groups[groupIndex].contacts.push(new Contact(newContact));
-    await User.findByIdAndUpdate(
-      { _id: userId },
-      {
-        $set: {
-          groups: currentUser.groups,
-        },
-      }
-    );
-  }
+	const currentUser = await User.findOne({ _id: userId });
+	const groupIndex = currentUser.groups.findIndex((group) => group._id == groupId);
+	if (groupIndex !== -1) {
+		currentUser.groups[groupIndex].contacts.push(new Contact(newContact));
+		await User.findByIdAndUpdate(
+			{ _id: userId },
+			{
+				$set: {
+					groups: currentUser.groups,
+				},
+			}
+		);
+	}
 
-  res.status(200).json(currentUser.groups[groupIndex]);
+	res.status(200).json(currentUser.groups[groupIndex]);
 }
 
 async function combineGroups(req, res) {
-  let { userId, groupName, groupA, groupB } = req.body;
-  let contacts = groupA.contacts.concat(groupB.contacts);
-  addGroup(userId, groupName, contacts, res);
+	let { userId, groupName, groupA, groupB } = req.body;
+	let contacts = groupA.contacts.concat(groupB.contacts);
+	addGroup(userId, groupName, contacts, res);
 }
 
 const putHandlers = {
-  addMessage,
-  editMessage,
-  uploadExcelfile,
-  editGroup,
-  addContactToGroup,
-  combineGroups,
+	addMessage,
+	editMessage,
+	uploadExcelfile,
+	editGroup,
+	addContactToGroup,
+	combineGroups,
 };
 
 module.exports = putHandlers;
