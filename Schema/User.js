273ada2e44861595b76398a05f2a82c5be426ce3
toam <toamcohen10@gmail.com>
2021-10-20 22:25:59 +0300

    i added new current group when we send from the electron that what we gonna catch

--- Schema/User.js
+++ Schema/User.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-	userName: { type: String },
-	password: { type: String },
-	messagesStatus: { type: Number },
-	groups: [],
-	messages: [],
-	history: [],
+  userName: { type: String },
+  password: { type: String },
+  messagesStatus: { type: Number },
+  groups: [],
+  messages: [],
+  history: [],
+  currentGroup: {},
 });
 
 const User = mongoose.model("users", userSchema);
