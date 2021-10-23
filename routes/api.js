8e7abc1011a175916f7a0b1d02fe3e6f49f083dd
toam <toamcohen10@gmail.com>
2021-10-23 19:00:50 +0300

    adding get fetch history

--- routes/api.js
+++ routes/api.js
@@ -4,22 +4,32 @@ const getHandlers = require("./get-handlers-functions");
 const postHandlers = require("./post-handlers-functions");
 const deleteHandlers = require("./delete-handlers-functions");
 const putHandlers = require("./put-handlers-functions");
-router.get('/', function (req, res) {
-	res.send("working good");
+router.get("/", function (req, res) {
+  res.send("working good");
 });
 
-router.post('/register', (req, res) => postHandlers.registerHandler(req, res));
-router.post('/login', (req, res) => postHandlers.loginHandler(req, res));
-router.put('/message', (req, res) => putHandlers.addMessage(req, res));
-router.put('/edit-message', (req, res) => putHandlers.editMessage(req, res));
-router.put('/group', (req, res) => putHandlers.uploadExcelfile(req, res));
-router.put('/combine-groups', (req, res) => putHandlers.combineGroups(req, res));
-router.put('/contact', (req, res) => putHandlers.addContactToGroup(req, res));
-router.put('/edit-group', (req, res) => putHandlers.editGroup(req, res));
-router.delete('/group', (req, res) => deleteHandlers.removeGroup(req, res));
-router.delete('/contact', (req, res) => deleteHandlers.removeContactFromGroup(req, res));
-router.delete('/message', (req, res) => deleteHandlers.deletemessage(req, res));
-router.get('/message/:userId', (req, res) => getHandlers.fetchMessages(req, res));
-router.get('/groups/:userId', (req, res) => getHandlers.fetchGroups(req, res));
+router.post("/register", (req, res) => postHandlers.registerHandler(req, res));
+router.post("/login", (req, res) => postHandlers.loginHandler(req, res));
+router.put("/message", (req, res) => putHandlers.addMessage(req, res));
+router.put("/edit-message", (req, res) => putHandlers.editMessage(req, res));
+router.put("/group", (req, res) => putHandlers.uploadExcelfile(req, res));
+router.put("/combine-groups", (req, res) =>
+  putHandlers.combineGroups(req, res)
+);
+router.put("/contact", (req, res) => putHandlers.addContactToGroup(req, res));
+router.put("/edit-group", (req, res) => putHandlers.editGroup(req, res));
+router.delete("/group", (req, res) => deleteHandlers.removeGroup(req, res));
+router.delete("/contact", (req, res) =>
+  deleteHandlers.removeContactFromGroup(req, res)
+);
+router.delete("/message", (req, res) => deleteHandlers.deletemessage(req, res));
+router.get("/message/:userId", (req, res) =>
+  getHandlers.fetchMessages(req, res)
+);
+router.get("/groups/:userId", (req, res) => getHandlers.fetchGroups(req, res));
+
+router.get("/history/:userId", (req, res) =>
+  getHandlers.fetchHistory(req, res)
+);
 
 module.exports = router;
