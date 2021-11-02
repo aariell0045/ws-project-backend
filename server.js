c6ca620c89272b21c66050dd6433f361d5cf5d94
toam <toamcohen10@gmail.com>
2021-11-03 00:14:55 +0200

    handled mongo error

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
-const mongooseURI = `mongodb+srv://toam:123987456tofo@ws.ppnha.mongodb.net/WS?retryWrites=true&w=majority`;
-mongoose.connect(
-  mongooseURI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => console.log("mongoose is connected")
-);
+const mongooseURI = `mongodb://localhost/wsDB`;
+// const mongooseURI = `mongodb+srv://toam:123987456tofo@ws.ppnha.mongodb.net/WS?retryWrites=true&w=majority`;
+mongoose.connect(mongooseURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const api = require("./routes/api");
+
+mongoose.connection
+  .once("open", () => console.log("connected"))
+  .on("error", (error) => {
+    console.log("YOUR ERROR ", error);
+  });
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
