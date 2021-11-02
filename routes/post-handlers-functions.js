124c177fa1594717133750050b47f53e58760d1a
toam <toamcohen10@gmail.com>
2021-11-02 19:21:19 +0200

    adding try catch to put handlers

--- routes/post-handlers-functions.js
+++ routes/post-handlers-functions.js
@@ -1,7 +1,5 @@
 const bcrypt = require("bcrypt");
-const Task = require("../Schema/Task");
 const User = require("../Schema/User");
-const Event = require("../Schema/Event");
 
 async function registerHandler(req, res) {
   let {
@@ -12,62 +10,68 @@ async function registerHandler(req, res) {
     messages = [],
     history = [],
   } = req.body;
+  try {
+    userName = userName.trim();
+    userName = userName.toLowerCase();
+    const usersCollection = await User.find();
 
-  userName = userName.trim();
-  userName = userName.toLowerCase();
-  const usersCollection = await User.find();
-
-  for (let user of usersCollection) {
-    if (user.userName === userName) {
-      return res.status(400).json("username is alredy token");
-    } else {
-      continue;
+    for (let user of usersCollection) {
+      if (user.userName === userName) {
+        return res.status(400).json("username is alredy token");
+      } else {
+        continue;
+      }
     }
-  }
 
-  const saltRounds = 10;
+    const saltRounds = 10;
 
-  if (password) {
-    bcrypt.genSalt(saltRounds, function (err, salt) {
-      bcrypt.hash(password, salt, async function (err, hash) {
-        let user = await new User({
-          userName,
-          password: hash,
-          messagesStatus: messagesStatus || 0,
-          groups,
-          messages,
-          history,
+    if (password) {
+      bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(password, salt, async function (err, hash) {
+          let user = await new User({
+            userName,
+            password: hash,
+            messagesStatus: messagesStatus || 0,
+            groups,
+            messages,
+            history,
+          });
+          await user.save();
+
+          // Store hash in your password DB.
         });
-        await user.save();
-
-        // Store hash in your password DB.
       });
-    });
-  } else {
-    return res.status(400).json("didnt found password");
-  }
+    } else {
+      return res.status(400).json("didnt found password");
+    }
 
-  res.status(200).json("success");
+    res.status(200).json("success");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function loginHandler(req, res) {
   let { userName, password } = req.body;
+  try {
+    userNmae = userName.trim();
+    userName = userName.toLowerCase();
 
-  userNmae = userName.trim();
-  userName = userName.toLowerCase();
+    let user = await User.findOne({ userName });
+    let hash = user.password;
 
-  let user = await User.findOne({ userName });
-  let hash = user.password;
-
-  bcrypt.compare(password, hash, function (error, result) {
-    if (result) {
-      return res.status(200).json({ result, userId: user._id });
-    } else {
-      return res
-        .status(200)
-        .json({ result: "username or password is incorrect", userId: null });
-    }
-  });
+    bcrypt.compare(password, hash, function (error, result) {
+      if (result) {
+        return res.status(200).json({ result, userId: user._id });
+      } else {
+        return res
+          .status(200)
+          .send({ result: "username or password is incorrect", userId: null });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 const postHandlers = {
