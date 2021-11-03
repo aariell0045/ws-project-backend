001d80c328a0fb97e849cd896dbb72f7491b5349
toam <toamcohen10@gmail.com>
2021-11-03 19:03:19 +0200

    remove mongoose error

--- server.js
+++ server.js
@@ -10,12 +10,6 @@ mongoose.connect(mongooseURI, {
   useUnifiedTopology: true,
 });
 
-mongoose.connection
-  .once("open", () => console.log("connected"))
-  .on("error", (error) => {
-    console.log("YOUR ERROR ", error);
-  });
-
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
